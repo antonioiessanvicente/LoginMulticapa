ea4cc563b8bf245762670478863397665d8983e1
Antonio Rodríguez <antonio@iessanvicente.com>
2015-11-07 12:47:01 +0100

    -

--- LoginMulticapa/Form1.vb
+++ LoginMulticapa/Form1.vb
@@ -13,7 +13,7 @@ Imports Npgsql
 Public Class Form1
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Dim bd As New BaseDeDatos
-        If bd.ComprobarLogin(TextBox1.Text, TextBox2.Text) > 0 Then
+        If Jugador.ComprobarLogin(TextBox1.Text, TextBox2.Text) > 0 Then
             MsgBox("Has accedido correctamente")
         Else
             MsgBox("El usuario y/o la contrseña introducidas son incorrectos")
@@ -21,7 +21,12 @@ Public Class Form1
     End Sub
 End Class
 
+''' <summary>
+''' 
+''' </summary>
+Public Class Negocio
 
+End Class
 ''' <summary>
 ''' Clase Jugador resumida y simplificada
 ''' </summary>
@@ -30,16 +35,21 @@ Public Class Jugador
     Public Property NICK_JUGADOR As String
     Public Property PASSWORD_JUGADOR As String
 
-    Public Sub New(ByVal pId As UInteger, ByVal pNick As Integer, ByVal pNom As String)
+    Public Sub New(ByVal pId As UInteger, ByVal pNick As String, ByVal pPass As String)
         Me.ID_JUGADOR = pId
-        Me.PASSWORD_JUGADOR = pNom
+        Me.NICK_JUGADOR = pNick
+        Me.PASSWORD_JUGADOR = pPass
     End Sub
+
     Public Overrides Function ToString() As String
         Return PASSWORD_JUGADOR
     End Function
 
-    Public Shared Function ComprobarLogin() As Boolean
+    Public Shared Function ComprobarLogin(ByVal pNick As String, ByVal pPass As String) As Integer
+        Dim bd As BaseDeDatos
+        bd = New BaseDeDatos
 
+        Return bd.ComprobarLogin(pNick, pPass)
 
     End Function
 
@@ -47,7 +57,7 @@ End Class
 
 
 ''' <summary>
-''' Clase base de datos
+''' Clase base de datos resumida y simplificada
 ''' </summary>
 Public Class BaseDeDatos
     Private ConexionConBD As NpgsqlConnection
@@ -76,11 +86,9 @@ Public Class BaseDeDatos
         End While
         'Llamar siempre a Close una vez finalizada la lectura
         Lector.Close()
+        CerrarConexion()
         Return resultado
     End Function
-
-
-
 
     Public Sub CerrarConexion()
         ' Cerrar la conexión cuando ya no sea necesaria
