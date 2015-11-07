671f1b12c91d51a430060eb86630a1ae7937ade8
Antonio Rodríguez <antonio@iessanvicente.com>
2015-11-07 12:31:47 +0100

    Primera versión

---
+++ LoginMulticapa/Form1.vb
@@ -0,0 +1,97 @@
+﻿Option Explicit On
+Option Strict On
+
+Imports System
+Imports System.Data
+Imports Npgsql
+
+
+''' <summary>
+''' Proyecto que implementa un logn multicapa. Cada una de las clases simula una de las capas, 
+''' pero por simplificar se añade todo en un único fichero y proyecto
+''' </summary>
+Public Class Form1
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim bd As New BaseDeDatos
+        If bd.ComprobarLogin(TextBox1.Text, TextBox2.Text) > 0 Then
+            MsgBox("Has accedido correctamente")
+        Else
+            MsgBox("El usuario y/o la contrseña introducidas son incorrectos")
+        End If
+    End Sub
+End Class
+
+
+''' <summary>
+''' Clase Jugador resumida y simplificada
+''' </summary>
+Public Class Jugador
+    Public Property ID_JUGADOR As UInteger
+    Public Property NICK_JUGADOR As String
+    Public Property PASSWORD_JUGADOR As String
+
+    Public Sub New(ByVal pId As UInteger, ByVal pNick As Integer, ByVal pNom As String)
+        Me.ID_JUGADOR = pId
+        Me.PASSWORD_JUGADOR = pNom
+    End Sub
+    Public Overrides Function ToString() As String
+        Return PASSWORD_JUGADOR
+    End Function
+
+    Public Shared Function ComprobarLogin() As Boolean
+
+
+    End Function
+
+End Class
+
+
+''' <summary>
+''' Clase base de datos
+''' </summary>
+Public Class BaseDeDatos
+    Private ConexionConBD As NpgsqlConnection
+    Private Orden As NpgsqlCommand
+    Private Lector As NpgsqlDataReader
+
+    Public Function ComprobarLogin(ByVal Usuario As String, ByVal pass As String) As Integer
+        Dim resultado As Integer = 0
+        Dim contrase As String = ""
+        'Abrir la base de datos
+        Dim strConexión As String = String.Format("Server={0};Port={1};User Id={2};Password={3};Database={4};",
+                                                  "LocalHost", "5432", "esports", "antonio", "esports")
+        ConexionConBD = New NpgsqlConnection(strConexión)
+        ConexionConBD.Open()
+
+        'Crear una consulta
+        Dim Consulta As String = "SELECT ID_JUGADOR, Password_jugador FROM Jugador WHERE Nick_jugador ='" & Usuario & "' AND Password_jugador = md5('" & pass & "')"
+        Orden = New NpgsqlCommand(Consulta, ConexionConBD)
+
+        'ExecuteReader hace la consulta y devuelve un OleDbDataReader
+        Lector = Orden.ExecuteReader()
+        'Llamar siempre a Read antes de acceder a los datos
+        While Lector.Read() 'siguiente registro
+            resultado = CInt(Lector("ID_JUGADOR"))
+            contrase = CStr("Password_jugador")
+        End While
+        'Llamar siempre a Close una vez finalizada la lectura
+        Lector.Close()
+        Return resultado
+    End Function
+
+
+
+
+    Public Sub CerrarConexion()
+        ' Cerrar la conexión cuando ya no sea necesaria
+        If (Not Lector Is Nothing) Then
+            Lector.Close()
+        End If
+        If (Not ConexionConBD Is Nothing) Then
+            ConexionConBD.Close()
+        End If
+    End Sub
+
+
+End Class
+
---
+++ LoginMulticapa/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("LoginMulticapa")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("LoginMulticapa")>
+<Assembly: AssemblyCopyright("Copyright ©  2015")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+<Assembly: Guid("744e63ba-a35b-4815-ae3b-32071afc4e09")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria 
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión 
+' mediante el carácter '*', como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
